31bd8db3e200809d2716d98e2d9972eb2cc16a8b
Danny Guinther <dannyguinther@gmail.com>
2016-03-18 22:40:52 -0400

    Expand on euler007

--- solutions/euler007/sieve/sieve.go
+++ solutions/euler007/sieve/sieve.go
@@ -1,63 +1,93 @@
 package sieve
 
 import (
+	//"fmt"
 	"github.com/willf/bitset"
 )
 
 const DefaultWindowSize = 10000
-const DefaultPrimeSliceSize = 1000
 
 func New(windowSize uint) *sieve {
-	sieve := new(sieve)
+	instance := new(sieve)
 	if windowSize != 0 {
-		sieve.windowSize = windowSize
+		instance.windowSize = windowSize
 	} else {
-		sieve.windowSize = DefaultWindowSize
+		instance.windowSize = DefaultWindowSize
 	}
-	sieve.initialize()
-	return sieve
+	instance.initialize()
+	return instance
 }
 
 type sieve struct {
 	cursorIndex  uint
-	primes       *[]uint
+	primes       []uint
 	primeCount   uint
 	window       *bitset.BitSet
 	windowOffset uint
 	windowSize   uint
+	windowSpan   uint
 }
 
 func (sieve *sieve) Ordinal(ordinal uint) uint {
-	if ordinal > uint(len(*sieve.primes)+1) {
-		sieve.scanUntil(ordinal)
+	if ordinal == 0 {
+		return 0
 	}
-	return (*sieve.primes)[ordinal]
+	for ordinal > sieve.primeCount {
+		sieve.findNext()
+	}
+	return sieve.primes[ordinal-1]
+}
+
+func (sieve *sieve) advanceCursor() {
+	if sieve.cursorIndex >= sieve.windowSize {
+		sieve.shiftWindow(sieve.windowOffset + sieve.windowSpan)
+		return
+	}
+
+	sieve.cursorIndex++
+}
+
+func (sieve *sieve) applyKnownPrimes() {
+	for i := uint(1); i < sieve.primeCount; i++ {
+		sieve.applyPrime(sieve.primes[i])
+	}
+}
+
+func (sieve *sieve) applyPrime(prime uint) {
+	cursorOffset := sieve.cursorValue()
+	firstRemainder := cursorOffset % (prime * 2)
+	applicationValue := cursorOffset - firstRemainder + prime
+	if firstRemainder > prime {
+		applicationValue += 2 * prime
+	}
+	applicationIndex := (applicationValue - sieve.windowOffset) / 2
+	for applicationIndex < sieve.windowSize {
+		sieve.window.Clear(applicationIndex)
+		applicationIndex += prime
+	}
+}
+
+func (sieve *sieve) cursorValue() uint {
+	return sieve.windowOffset + (2 * sieve.cursorIndex)
+}
+
+func (sieve *sieve) findNext() {
+	for !sieve.window.Test(sieve.cursorIndex) {
+		sieve.advanceCursor()
+	}
+	prime := sieve.cursorValue()
+	sieve.primes = append(sieve.primes, prime)
+	sieve.primeCount++
+	sieve.applyPrime(prime)
+	sieve.advanceCursor()
 }
 
 func (sieve *sieve) initialize() {
-	primeSlice := make([]uint, DefaultPrimeSliceSize)
-	sieve.primes = &primeSlice
+	sieve.primes = []uint{2}
 	sieve.window = bitset.New(sieve.windowSize)
-	sieve.shiftWindow(2)
-	primeSlice[0] = 2
-	sieve.primeCount = uint(1)
-}
-
-func (sieve *sieve) scanUntil(ordinal uint) {
-	for ordinal > sieve.primeCount {
-		sieve.checkNext()
-	}
-}
-
-func (sieve *sieve) checkNext() {
-	for sieve.cursorIndex < sieve.windowSize {
-		if !sieve.window.Test(sieve.windowOffset) {
-			sieve.cursorIndex++
-			continue
-		}
-		(*sieve.primes)[sieve.primeCount] = sieve.windowOffset
-		sieve.primeCount++
-	}
+	sieve.windowSpan = sieve.windowSize * 2
+	sieve.primeCount = 1
+	sieve.shiftWindow(3)
 }
 
 func (sieve *sieve) shiftWindow(offset uint) {
@@ -67,29 +97,4 @@ func (sieve *sieve) shiftWindow(offset uint) {
 	sieve.windowOffset = offset
 	sieve.cursorIndex = uint(0)
 	sieve.applyKnownPrimes()
-}
-
-func (sieve *sieve) advanceCursor() {
-	if sieve.cursorIndex >= sieve.windowSize {
-		sieve.shiftWindow(sieve.windowOffset + sieve.windowSize)
-		return
-	}
-
-	sieve.cursorIndex++
-	sieve.windowOffset++
-}
-
-func (sieve *sieve) applyKnownPrimes() {
-	for i := uint(0); i < sieve.primeCount; i++ {
-		prime := (*sieve.primes)[i]
-		firstRemainder := sieve.windowOffset % prime
-		firstInWindow := sieve.windowOffset - firstRemainder
-		if firstRemainder > 0 {
-			firstInWindow = firstInWindow + prime
-		}
-		firstIndex := firstInWindow - sieve.windowOffset
-		for j := firstIndex; j < sieve.windowSize; j = j + prime {
-			sieve.window.Clear(j)
-		}
-	}
 }
